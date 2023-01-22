55bd90211ade618ded43ae0637e0d7a7ed75eaff
nishkarsh1512 <121868039+nishkarsh1512@users.noreply.github.com>
2023-01-22 17:13:27 +0530

    Add files via upload

---
+++ router/campgrounds.js
@@ -0,0 +1,81 @@
+const express = require('express')
+const router = express.Router()
+const mongoose = require('mongoose')
+////////////////////////////////////////////////////////////////////
+const dbUrl = process.env.DB_URL
+
+mongoose.connect(dbUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', () => {
+  console.log('Database connected')
+})
+
+////////////////////////////////////////////////////////////////////////////
+const Campground = require('../models/campground.js')
+const catchAsync = require('../utils/catchAsync.js')
+const campgrounds = require('../controllers/campground.js')
+
+const { isLoggedIn, isAuthor, validateCampground } = require('../middleware.js')
+const multer = require('multer')
+const { storage } = require('../cloudinary/index.js')
+const upload = multer({ storage })
+
+/////////////////////////////////////////////////////////////////
+router.get('/campground', async function (req, res, next) {
+  const campgrounds = await Campground.find({})
+  res.render('index', { campgrounds })
+})
+
+////////////////////////////////////////////////////////////////////
+
+router
+  .route('/campground')
+  .post(
+    isLoggedIn,
+    upload.array('image'),
+    validateCampground,
+    catchAsync(campgrounds.creat),
+  )
+
+//////////////////////////////////////////////////////////////////////
+
+router.get('/', function(req, res){
+  res.render('home');
+})
+
+router.get('/campground/new', isLoggedIn, (req, res) => {
+  res.render('new')
+})
+
+//////////////////////////////////////////////////////////////////////
+router
+  .route('/campground/:id')
+  .put(
+    isLoggedIn,
+    isAuthor,
+    upload.array('image'),
+    validateCampground,
+    campgrounds.a3,
+  )
+  .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.del))
+  .get(catchAsync(campgrounds.a4))
+
+//////////////////////////////////////////////////////////////////////
+
+router.get('/campground/:id/edit', isLoggedIn, isAuthor, campgrounds.edits)
+
+///////////////////////////////////////
+router.get('/campground/:id/request', async function (req, res) {
+  const { id } = req.params
+  console.log(`hello ${req.body}`)
+})
+
+router.post('/campground/:id/', campgrounds.a5)
+////////////////////////////////////////////////////////////////////////////
+
+module.exports = router
